57838ee9a22572a5426ee6100dc06f816e63deca
VladZinko <zinko1996@yandex.ru>
2024-08-22 13:42:54 +0300

    added dependency injection of OrderService in OrderController with constructor and added calling of service methods

--- src/main/java/com/example/techtask/controller/OrderController.java
+++ src/main/java/com/example/techtask/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.example.techtask.controller;
 
 import com.example.techtask.model.Order;
+import com.example.techtask.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,18 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("api/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
-  // DI here
+  private final OrderService orderService;// DI here
 
   @GetMapping("desired-order")
   public Order findOrder() {
-    return null;
+    return orderService.findOrder();
   }
 
   @GetMapping("desired-orders")
   public List<Order> findOrders() {
-    return null;
+    return orderService.findOrders();
   }
 }
