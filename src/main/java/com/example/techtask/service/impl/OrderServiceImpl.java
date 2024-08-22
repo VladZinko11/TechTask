5fa21df427690043270e17209582bae7a132551f
VladZinko <zinko1996@yandex.ru>
2024-08-22 20:03:48 +0300

    added DI OrderRepository and added calling of methods

--- src/main/java/com/example/techtask/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/techtask/service/impl/OrderServiceImpl.java
@@ -1,20 +1,28 @@
 package com.example.techtask.service.impl;
 
+import com.example.techtask.data.repository.OrderRepository;
 import com.example.techtask.model.Order;
 import com.example.techtask.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
     @Override
     public Order findOrder() {
-        return null;
+        Optional<Order> optionalOrder = orderRepository.findLastWithMoreThanOneProduct();
+        return optionalOrder.orElseThrow(()->new RuntimeException("Not found order with more than one product"));
     }
 
     @Override
     public List<Order> findOrders() {
-        return null;
+        return orderRepository.findByActiveUsersOrderByDate();
     }
 }
