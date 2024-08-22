86de10a1347b57d0a57934f83abb01be6a711b8d
VladZinko <zinko1996@yandex.ru>
2024-08-22 20:03:30 +0300

    added DI UserRepository and added calling of methods

--- src/main/java/com/example/techtask/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/techtask/service/impl/UserServiceImpl.java
@@ -1,20 +1,28 @@
 package com.example.techtask.service.impl;
 
+import com.example.techtask.data.repository.UserRepository;
 import com.example.techtask.model.User;
 import com.example.techtask.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
     @Override
     public User findUser() {
-        return null;
+        Optional<User> optionalUser = userRepository.findByMaxSumOrdersPriceIn2003();
+        return optionalUser.orElseThrow(()-> new RuntimeException("Not found user "));
     }
 
     @Override
     public List<User> findUsers() {
-        return null;
+        return userRepository.findAllByPaidOrdersIn2010();
     }
 }
