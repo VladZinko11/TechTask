a562f6d716805ba658bded8318999ab51d9b05aa
VladZinko <zinko1996@yandex.ru>
2024-08-22 13:38:06 +0300

    created two classes implementations of UserService and OrderService - UserServiceImpl and OrderServiceImpl

---
+++ src/main/java/com/example/techtask/service/impl/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.techtask.service.impl;
+
+import com.example.techtask.model.Order;
+import com.example.techtask.service.OrderService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order findOrder() {
+        return null;
+    }
+
+    @Override
+    public List<Order> findOrders() {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/techtask/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.techtask.service.impl;
+
+import com.example.techtask.model.User;
+import com.example.techtask.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User findUser() {
+        return null;
+    }
+
+    @Override
+    public List<User> findUsers() {
+        return null;
+    }
+}
